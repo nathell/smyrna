3ec50f78c72431697fdd728b76744da0143df487
Daniel Janus <dj@danieljanus.pl>
2010-09-07 21:47:47 +0200

    Initial project structure.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject smyrna "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                   [org.clojure/clojure-contrib "1.2.0"]])
---
+++ src/smyrna/core.clj
@@ -0,0 +1,1 @@
+(ns smyrna.core)
