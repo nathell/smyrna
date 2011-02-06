a56d308f348b276b46b6e2d66360d32a91158f10
Daniel Janus <dj@danieljanus.pl>
2011-02-06 20:49:37 +0100

    Use clj-tagsoup 0.2.4.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojars.nathell/morfologik-stemming "1.4.0-cidict1.6"]
-                 [clj-tagsoup "0.2.2"]
+                 [clj-tagsoup "0.2.4"]
                  [hiccup "0.3.1"]
                  [ring "0.3.5"]]
   :dev-dependencies [[swank-clojure "1.2.0"]])
