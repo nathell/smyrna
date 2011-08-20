2a514d5fcf59fe04199c9607ccb00f8c2c035e4f
Daniel Janus <dj@danieljanus.pl>
2011-08-20 14:51:40 +0200

    Update dependencies, bump version to 0.2.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject smyrna "0.1"
-  :description "FIXME: write"
+(defproject smyrna "0.2"
+  :description "A simple concordancer for Polish corpora."
   :main smyrna.main
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.nathell/morfologik-stemming "1.4.0-cidict1.6"]
-                 [clj-tagsoup "0.2.4"]
+                 [org.clojars.nathell/morfologik-stemming "1.5.1-cidict1.8.1"]
+                 [clj-tagsoup "0.2.6"]
                  [hiccup "0.3.1"]
                  [clojure-csv/clojure-csv "1.3.1"]
                  [ring "0.3.5"]]
